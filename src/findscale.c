e8fb1fc3ab0cf741e48cbe19e6265f9f59c20c9e
Joe Staursky <jstaursky@gmail.com>
2019-05-20 14:14:29 -0400

    Fixed freeze on exit, added select scale widget, still need to add signal/event trigger to update instrument graphics to match scale selected inside scale widget.

--- src/findscale.c
+++ src/findscale.c
@@ -9,17 +9,17 @@
 #include "CircularLinkedList.h"
 #include "gtk/gtkwidget.h"
 
+#define DEBUG_CHECK
+
 struct scale_t {
     char *name;
     struct node_t *scale;
     int num_notes;
 };
-
 struct database_t {
     struct scale_t **entry;
     int size;
 };
-
 struct imagelayers_t {
     cairo_surface_t *instrument;
     cairo_surface_t *notes[12]; // chromaticscale is largest scale.
@@ -28,49 +28,73 @@ struct imagelayers_t {
 
 
 struct database_t *build_database(FILE *);
-
 struct imagelayers_t *getimages(struct scale_t *, char *, char *);
-
 static gboolean on_draw_event(GtkWidget *, cairo_t *, gpointer fn_parameter);
-
 char *fgetline(FILE *stream);
 
 
 int main(int argc, char *argv[])
 {
-    // Create window, set position, title, etc.
+    // Create window, set position, title, etc. reference
+    // https://developer.gnome.org/gtk-tutorial/stable/c450.html for more info
+    // on creating widgets.
     gtk_init(&argc, &argv);
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_default_size(GTK_WINDOW(window), 300, 220);
     gtk_window_set_title(GTK_WINDOW(window), "Findscale");
-    
+    // Handle exit signal.
+    g_signal_connect(G_OBJECT(window), "destroy",
+                     G_CALLBACK(gtk_main_quit), NULL);
+
+    // Create container for packing widgets in a single row.
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+    gtk_container_add(GTK_CONTAINER(window), box);
+
     // Create draw area.
+    // reference https://developer.gnome.org/gtk3/stable/GtkDrawingArea.html for
+    // more info on gtk_drawing_area_new()
     GtkWidget *draw_area = gtk_drawing_area_new();
-    gtk_container_add(GTK_CONTAINER(window), draw_area);
-    
+    gtk_box_pack_start(GTK_BOX(box), draw_area, TRUE, TRUE, 10);
+
     // Import list of scales.
     FILE *configfp = fopen("conf/scale.list", "r");
     struct database_t *scale_database = build_database(configfp);
-    
+
+#ifdef DEBUG_CHECK
     // Print results of import.
     for (int i = 0; i < scale_database->size; ++i) {
         circularlist_traverse(scale_database->entry[i]->scale,
                               circularlist_print);
     }
+#endif
     // Get the images associated with database entry 0.
-    struct imagelayers_t *displaylayers =
-        getimages(scale_database->entry[0], argv[1], argv[2]);
-    
+    struct imagelayers_t *displaylayers = getimages(scale_database->entry[0],
+                                                    argv[1], argv[2]);
+
     // Draw the image layers just received which correspond to database entry 0.
-    g_signal_connect(G_OBJECT(draw_area), "draw", *G_CALLBACK(on_draw_event),
-                     (gpointer)displaylayers);
+    // reference https://developer.gnome.org/gtk-tutorial/stable/x159.html for
+    // more info on 'g_signal_connect'.
+    g_signal_connect(G_OBJECT(draw_area), "draw",
+                     *G_CALLBACK(on_draw_event), (gpointer)displaylayers);
+
+
+    // Create widget to choose scale from database.
+    GtkWidget *list_box = gtk_combo_box_text_new();
+    gtk_box_pack_start(GTK_BOX(box), list_box, FALSE, TRUE, 0);
+    for (int i = 0; i < scale_database->size; ++i) {
+        gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(list_box),
+                                       scale_database->entry[i]->name);
+    }
+
 
     /* Begin running of GUI. ----------------------------------------- */
     gtk_widget_show_all(window);
     gtk_main();
     return 0;
 }
+
+
 
 struct database_t *build_database(FILE *fp)
 {
@@ -152,11 +176,13 @@ struct imagelayers_t *getimages(struct scale_t *scale_p, char *instrument_name,
 }
 
 /*
+ * Callback function used with g_signal_connect().
  * Redraw the screen from the surface. Note that the ::draw
  * signal receives a ready-to-be-used cairo_t that is already
  * clipped to only draw the exposed areas of the widget
  */
-static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr,
+static gboolean on_draw_event(GtkWidget *widget, // Widget emitting a signal.
+                              cairo_t *cr,       // Signal being emitted.
                               gpointer fn_parameter)
 {
     struct imagelayers_t *layers = fn_parameter;
